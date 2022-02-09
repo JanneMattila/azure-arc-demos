7b4e81ab56d12cc74f266cceda7c7c9dec5fd10e
Janne Mattila <janne.mattila@hotmail.fi>
2022-02-09 14:52:43 +0200

    Update demo

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -37,26 +37,65 @@ echo $workspaceid
 
 kubectl get nodes
 kubectl get deployments,pods -n azure-arc
+#
+# NAME                                        READY   UP-TO-DATE   AVAILABLE   AGE
+# deployment.apps/cluster-metadata-operator   1/1     1            1           6m31s
+# deployment.apps/clusterconnect-agent        1/1     1            1           6m31s
+# deployment.apps/clusteridentityoperator     1/1     1            1           6m31s
+# deployment.apps/config-agent                1/1     1            1           6m31s
+# deployment.apps/controller-manager          1/1     1            1           6m31s
+# deployment.apps/extension-manager           1/1     1            1           6m31s
+# deployment.apps/flux-logs-agent             1/1     1            1           6m31s
+# deployment.apps/kube-aad-proxy              1/1     1            1           6m31s
+# deployment.apps/metrics-agent               1/1     1            1           6m31s
+# deployment.apps/resource-sync-agent         1/1     1            1           6m31s
+# 
+# NAME                                             READY   STATUS    RESTARTS   AGE
+# pod/cluster-metadata-operator-7d9b465454-tsqc4   2/2     Running   0          6m31s
+# pod/clusterconnect-agent-b65cbd6b5-nbl6p         3/3     Running   0          6m31s
+# pod/clusteridentityoperator-657fd75459-vk76l     2/2     Running   0          6m31s
+# pod/config-agent-b6547d575-mkhtz                 2/2     Running   0          6m31s
+# pod/controller-manager-66d56c9bf-9qzbn           2/2     Running   0          6m31s
+# pod/extension-manager-cf49d5cbd-rqvn8            2/2     Running   0          6m31s
+# pod/flux-logs-agent-6cbd59f69d-js7pd             1/1     Running   0          6m31s
+# pod/kube-aad-proxy-65bf446c-hzwz5                2/2     Running   0          6m31s
+# pod/metrics-agent-667587f7bd-h6jtv               2/2     Running   0          6m31s
+# pod/resource-sync-agent-54b8f8c755-5gl28         2/2     Running   0          6m31s
+# 
 
-monitorExtensionId = $(az k8s-extension create `
-  --name azuremonitor-containers `
-  --cluster-name $arcName `
-  --resource-group $resourceGroupName `
-  --cluster-type connectedClusters `
-  --extension-type Microsoft.AzureMonitor.Containers `
+# Note: After above you can already see "Standard" Metrics
+# at the portal e.g., Total number of cpu cores in a connected cluster
+# but not yet Insights or Logs.
+
+monitorExtensionId=$(az k8s-extension create \
+  --name azuremonitor-containers \
+  --cluster-name $arcName \
+  --resource-group $resourceGroupName \
+  --cluster-type connectedClusters \
+  --extension-type Microsoft.AzureMonitor.Containers \
   --configuration-settings logAnalyticsWorkspaceResourceID=$workspaceid)
 
-az resource wait --ids $monitorExtensionId --custom "properties.installState!='Pending'" --api-version "2020-07-01-preview"
+az k8s-extension show \
+  --name azuremonitor-containers \
+  --cluster-name $arcName \
+  --resource-group $resourceGroupName \
+  --cluster-type connectedClusters \
+  -n azuremonitor-containers
+
+kubectl get deployments -n kube-system
+# NAME          READY   UP-TO-DATE   AVAILABLE   AGE
+# coredns       2/2     2            2           9d
+# omsagent-rs   1/1     1            1           8m37s
 
 # Apply log and prometheus data collection settings 
-kubectl apply -f container-azm-ms-agentconfig.yaml
+kubectl apply -f k8s/container-azm-ms-agentconfig.yaml
 
 # Create namespace
-kubectl apply -f namespace.yaml
+kubectl apply -f k8s/namespace.yaml
 
 # Create deployment & service
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
 
 kubectl get deployment -n demos
 kubectl describe deployment -n demos
@@ -69,13 +108,20 @@ kubectl describe $pod1 -n demos
 
 kubectl get service -n demos
 
-ingressip=$(kubectl get service -n demos -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
-echo $ingressip
+# 1. You can use port forwarding
+kubectl port-forward service/webapp-monitoring-demo -n demos 1080:80
+# Navigate:
+# http://localhost:1080/
+# http://localhost:1080/metrics
 
-curl $ingressip
-curl $ingressip/home/privacy
-curl $ingressip/notfound
-curl $ingressip/metrics
+# 2. You can use exposed node port
+svc=localhost:30080
+echo $svc
+
+curl $svc
+curl $svc/home/privacy
+curl $svc/notfound
+curl $svc/metrics
 
 # Query prometheus logs from our test app
 workspaceCustomerId=$(az monitor log-analytics workspace create --resource-group $resourceGroupName --workspace-name $workspaceName --query customerId -o tsv)

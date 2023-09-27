01de5b24f67a607c034e82bc1d78722fa2761e1d
Janne Mattila <janne.mattila@hotmail.fi>
2023-09-27 13:55:44 +0300

    Enhance deployment script

--- hci/deploy.ps1
+++ hci/deploy.ps1
@@ -1,4 +1,42 @@
+# Use deployment folder
 Set-Location hci
+
+############################################### 
+#  ____
+# |  _ \ _   _ _ __     ___  _ __   ___ ___
+# | |_) | | | | '_ \   / _ \| '_ \ / __/ _ \
+# |  _ <| |_| | | | | | (_) | | | | (_|  __/
+# |_| \_\\__,_|_| |_|  \___/|_| |_|\___\___|
+# to create deployment service principal
+############################################### 
+#region Create deployment service principal
+Connect-AzAccount
+
+# *Explicitly* select your working context
+Select-AzSubscription -Subscription "<YourSubscriptionName>"
+
+$context = Get-AzContext
+$subscription = $context.Subscription.Id
+
+$newSPN = New-AzADServicePrincipal -DisplayName "HCIBoxSPN" -Role "Owner" -Scope "/subscriptions/$($subscription)"
+
+# Save the service principal to a JSON file
+$newSPN | ConvertTo-Json > spn.json
+
+#endregion
+
+###############################################
+#  ____             _
+# |  _ \  ___ _ __ | | ___  _   _
+# | | | |/ _ \ '_ \| |/ _ \| | | |
+# | |_| |  __/ |_) | | (_) | |_| |
+# |____/ \___| .__/|_|\___/ \__, |
+#            |_|            |___/
+# HCIBox demo environment
+###############################################
+
+# Now you restore the service principal from the JSON file
+$spn = Get-Content spn.json | ConvertFrom-Json
 
 # Download deployment scripts
 Invoke-WebRequest -Uri "https://github.com/microsoft/azure_arc/archive/main.zip" -OutFile azure_arc.zip
@@ -6,8 +44,21 @@ Expand-Archive azure_arc.zip -DestinationPath azure_arc -Force
 
 Set-Location azure_arc/azure_arc-main/azure_jumpstart_hcibox/bicep
 
-# Edit parameters
-code main.parameters.json
+# Create copy of original parameters file
+Copy-Item main.parameters.json main.parameters.json.bak
+
+# Update deployment parameters
+$configuration = Get-Content main.parameters.json | ConvertFrom-Json
+$configuration.parameters.spnClientId.value = $spn.AppId
+$configuration.parameters.spnClientSecret.value = $spn.PasswordCredentials.SecretText
+$configuration.parameters.spnTenantId.value = $spn.AppOwnerOrganizationId
+$configuration | ConvertTo-Json > main.parameters.json
+
+$configuration.parameters
+
+$clientPassword = ConvertTo-SecureString $spn.PasswordCredentials.SecretText -AsPlainText -Force
+$credentials = New-Object System.Management.Automation.PSCredential($spn.AppId, $clientPassword)
+Connect-AzAccount -Credential $credentials -ServicePrincipal -TenantId $spn.AppOwnerOrganizationId
 
 # Ensure context
 Get-AzContext
@@ -25,8 +76,6 @@ $result = New-AzResourceGroupDeployment `
     -Verbose
 
 $result
-
-$configuration = Get-Content main.parameters.json | ConvertFrom-Json
 
 # Enable access to RDP from this machine
 $myIP = Invoke-RestMethod https://myip.jannemattila.com
